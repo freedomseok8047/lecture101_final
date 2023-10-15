4ae8ebc39b7ffb5c4704f7671dc92b7d4a606caf
김태빈 <tb081212@naver.com>
2023-10-15 15:51:00 +0900

    선택 날짜 저장

--- src/main/java/com/lecture101/repository/CartItemRepository.java
+++ src/main/java/com/lecture101/repository/CartItemRepository.java
@@ -12,15 +12,16 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 
-    @Query("select new com.lecture101.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+    @Query("select new com.lecture101.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl, FUNCTION('DATE_FORMAT', ci.selectedDate, '%Y-%m-%d')) " +
             "from CartItem ci, ItemImg im " +
             "join ci.item i " +
             "where ci.cart.id = :cartId " +
             "and im.item.id = ci.item.id " +
             "and im.repimgYn = 'Y' " +
             "order by ci.regTime desc"
-            )
+    )
     List<CartDetailDto> findCartDetailDtoList(Long cartId);
+
     List<CartItem> findBySelectedDate(LocalDate selectedDate);
 
 }
